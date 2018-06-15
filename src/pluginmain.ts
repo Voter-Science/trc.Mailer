f959b8c79324041723aed442bc64a8d7279eb7ba
Voter-Science <Info@Voter-Science.com>
2018-06-14 23:14:50 -0700

    Initial add

--- src/pluginmain.ts
+++ src/pluginmain.ts
@@ -11,12 +11,15 @@ import * as common from 'trc-httpshim/common'
 
 import * as core from 'trc-core/core'
 
-import * as trcSheet from 'trc-sheet/sheet' 
+import * as trcSheet from 'trc-sheet/sheet'
 import * as trcSheetEx from 'trc-sheet/sheetEx'
+import { ISheetContents, ColumnNames, SheetContents } from 'trc-sheet/sheetContents'
 
-import * as gps from 'trc-web/gps'
 import * as plugin from 'trc-web/plugin'
 import * as trchtml from 'trc-web/html'
+
+import * as bcl from 'trc-analyze/collections'
+import * as hh from 'trc-analyze/household'
 
 
 // Installed via:
@@ -26,94 +29,107 @@ declare var $: JQueryStatic;
 
 // Provide easy error handle for reporting errors from promises.  Usage:
 //   p.catch(showError);
-declare var showError : (error:any) => void; // error handler defined in index.html
+declare var showError: (error: any) => void; // error handler defined in index.html
 
 export class MyPlugin {
     private _sheet: trcSheet.SheetClient;
-    private _pluginClient : plugin.PluginClient;
-    private _gps : common.IGeoPointProvider;
+    private _pluginClient: plugin.PluginClient;
 
     public static BrowserEntryAsync(
         auth: plugin.IStart,
-        opts : plugin.IPluginOptions
-    ) : Promise<MyPlugin> {
-        
-        // You can set gpsTracker null if you don't need GPS. 
-        var gpsTracker = new gps.GpsTracker(); // Only works in browser
-        var pluginClient = new plugin.PluginClient(auth,opts, gpsTracker);
+        opts: plugin.IPluginOptions
+    ): Promise<MyPlugin> {
+
+        var pluginClient = new plugin.PluginClient(auth, opts);
 
         // Do any IO here...
-        
-        var throwError =false; // $$$ remove this
-        
+
+        var throwError = false; // $$$ remove this
+
         var plugin2 = new MyPlugin(pluginClient);
-        plugin2._gps = gpsTracker;
-        return plugin2.InitAsync().then( () => {
+        return plugin2.InitAsync().then(() => {
             if (throwError) {
                 throw "some error";
             }
-            
-            gpsTracker.start((loc) => plugin2.OnGpsChanged(loc)); // ignore callback
-            return plugin2;                        
+
+            return plugin2;
         });
     }
 
-    private OnGpsChanged(loc : gps.IGeoPoint) : void {
-        // Notification that the GPS position has change. 
-        // Use this if you have a map view showing the user's "current location"
-        var msg = "(Lat: " + loc.Lat + ", Long:" + loc.Long + ")";
-        $("#locInfo").text(msg);
-    }
-
     // Expose constructor directly for tests. They can pass in mock versions. 
-    public constructor(p : plugin.PluginClient) {
+    public constructor(p: plugin.PluginClient) {
         this._sheet = new trcSheet.SheetClient(p.HttpClient, p.SheetId);
     }
-    
+
 
     // Make initial network calls to setup the plugin. 
     // Need this as a separate call from the ctor since ctors aren't async. 
-    private InitAsync() : Promise<void> {
-        return this._sheet.getInfoAsync().then( info  => {
+    private InitAsync(): Promise<void> {
+        return this._sheet.getInfoAsync().then(info => {
             this.updateInfo(info);
-        });     
+            
+            return this._sheet.getSheetContentsAsync().then((contents) => {
+
+                var mailList = MyPlugin.createMailList(contents);
+
+                var e = document.getElementById("download");
+                trchtml.DownloadHelper.appendDownloadCsvButton(e, () => mailList);
+            })
+        });
     }
 
     // Display sheet info on HTML page
     public updateInfo(info: trcSheet.ISheetInfoResult): void {
         $("#SheetName").text(info.Name);
-        $("#ParentSheetName").text(info.ParentName);
-        $("#SheetVer").text(info.LatestVersion);
-        $("#RowCount").text(info.CountRecords);
-
-        $("#LastRefreshed").text(new Date().toLocaleString());
+        $("#RowCount").text(info.CountRecords.toLocaleString());
     }
 
-    // Example of a helper function.
-    public doubleit(val: number): number {
-        return val * 2;
-    }
 
     // Demonstrate receiving UI handlers 
     public onClickRefresh(): void {
-        this.InitAsync().        
+        this.InitAsync().
             catch(showError);
     }
 
 
-    // downloading all contents and rendering them to HTML can take some time. 
-    public onGetSheetContents(): void {
-        trchtml.Loading("contents");
-        //$("#contents").empty();
-        //$("#contents").text("Loading...");
+    public static createMailList(contents: ISheetContents): ISheetContents {
+        var x: hh.IHousheholding = new hh.Householding(contents);
 
-        trcSheetEx.SheetEx.InitAsync(this._sheet, this._gps).then((sheetEx)=>
-        {
-            return this._sheet.getSheetContentsAsync().then((contents) => {
-                var render = new trchtml.SheetControl("contents", sheetEx);
-                // could set other options on render() here
-                render.render();
-            }).catch(showError);
-        });        
+        var colRecId = contents[ColumnNames.RecId];
+        var colFirstName = contents[ColumnNames.FirstName];
+        var colLastName = contents[ColumnNames.LastName];
+        var colAddress = contents[ColumnNames.Address];
+        var colCity = contents[ColumnNames.City];
+        var colZip = contents[ColumnNames.Zip];
+
+        var mailer: ISheetContents = {}
+
+        mailer[ColumnNames.FirstName] = colFirstName;
+        mailer[ColumnNames.LastName] = colLastName;
+        mailer[ColumnNames.Address] = colAddress;
+        mailer[ColumnNames.City] = colCity;
+        mailer[ColumnNames.Zip] = colZip;
+
+
+        var unique: any = {}
+
+        var count = 0;
+
+        var mailer2 = SheetContents.KeepRows(mailer, (i) => {
+            var hhid = hh.Householding.calcHHID(colAddress[i], colCity[i], colZip[i]);
+
+            if (unique.hasOwnProperty(hhid)) {
+                return false; // already present. 
+            } else {
+                unique[hhid] = true;
+                count++;
+                return true;
+            }
+        })
+
+        $("#HouseholdCount").text(count.toLocaleString());
+
+        return mailer2;
+
     }
 }
